db266c304bbc2d01f07f10a8123d9043861c2e65
Khosro <Jadidi>
2020-04-22 23:51:41 +0200

    adding navigation.incomplete

--- Lab3-Berras-Bio-version4/Lab3-Berras-Bio-version4/Controllers/HomeController.cs
+++ Lab3-Berras-Bio-version4/Lab3-Berras-Bio-version4/Controllers/HomeController.cs
@@ -12,19 +12,19 @@ namespace Lab3_Berras_Bio_version4.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IMovieRepository movieRepository;
+        //private readonly IMovieRepository movieRepository;
         private readonly IShowingRepository showingRepository;
-        private readonly ITicketRepository ticketRepository;
-        private readonly IUserRepository userRepository;
+        //private readonly ITicketRepository ticketRepository;
+        //private readonly IUserRepository userRepository;
         public HomeController(IMovieRepository movieRepository,
                                 IShowingRepository showingRepository,
                                 ITicketRepository ticketRepository,
                                 IUserRepository userRepository)
         {
-            this.movieRepository = movieRepository;
+            //this.movieRepository = movieRepository;
             this.showingRepository = showingRepository;
-            this.ticketRepository = ticketRepository;
-            this.userRepository = userRepository;
+            //this.ticketRepository = ticketRepository;
+            //this.userRepository = userRepository;
         }
 
         public ViewResult Index()
@@ -32,6 +32,16 @@ namespace Lab3_Berras_Bio_version4.Controllers
             HomeViewModel homeViewModel = new HomeViewModel();
             homeViewModel.Showings = showingRepository.Allshowings;
             return View(homeViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult BookTicket(int id)
+        {
+            //get user
+            var showing = showingRepository.GetShowingById(id);
+            //create ticket
+            //https://stackoverflow.com/questions/30020892/taghelper-for-passing-route-values-as-part-of-a-link
+            return View(showing);
         }
     }
 }
