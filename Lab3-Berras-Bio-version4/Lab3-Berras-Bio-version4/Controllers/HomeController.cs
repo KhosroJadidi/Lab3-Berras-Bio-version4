9d4b386bbfc31b8fcec5a938de39cb0585011419
Khosro <Jadidi>
2020-05-03 02:53:38 +0200

    added preparation for sorting

--- Lab3-Berras-Bio-version4/Lab3-Berras-Bio-version4/Controllers/HomeController.cs
+++ Lab3-Berras-Bio-version4/Lab3-Berras-Bio-version4/Controllers/HomeController.cs
@@ -34,40 +34,5 @@ namespace Lab3_Berras_Bio_version4.Controllers
             };
             return View(homeViewModel);
         }
-
-        //[HttpPost]
-        //public ActionResult RedirectToOnPostBookTicket( int showingId )
-        //{
-        //    //https://stackoverflow.com/questions/1257482/redirecttoaction-with-parameter
-
-        //    //var name = new ClaimsPrincipal(User).Identity.Name;
-        //    //var userId = _appDbContext.Users.FirstOrDefault(user => user.UserName == name).Id;
-
-
-        //    return RedirectToAction(actionName: "OnPostBookTicket",
-        //        controllerName: "Ticket",
-        //        routeValues: new { showingId = showingId });
-        //}
-        //[Authorize]
-        //[HttpPost]
-        //public ActionResult ThisIsATest(int showingId)
-        //{
-        //    var userName = new ClaimsPrincipal(User).Identity.Name;
-        //    var user = _appDbContext.Users.FirstOrDefault(user => user.UserName == userName);
-
-        //    var showing = _appDbContext.Showings.
-        //        Include(showing => showing.Movie).
-        //        Include(showing => showing.Auditorium).
-        //        FirstOrDefault(showingToSelect => showingToSelect.Id == showingId);
-
-        //    //create ticket
-        //    //https://stackoverflow.com/questions/30020892/taghelper-for-passing-route-values-as-part-of-a-link
-
-        //    var ticket = new Ticket { Showing = showing, User = user };
-
-        //    _appDbContext.Tickets.Add(ticket);
-        //    _appDbContext.SaveChanges();
-        //    return View(ticket);
-        //}
     }
 }
